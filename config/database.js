c09d2bc1c522e97809c9af605ae3d439e1d2d8b5
VladimirGostik <xgostik@stuba.sk>
2025-01-30 19:57:05 +0100

    update

--- config/database.js
+++ config/database.js
@@ -1,16 +1,14 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config(); // Načítanie .env súboru
 
-// Inicializácia Sequelize s PostgreSQL pomocou údajov z .env
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false // Dôležité pre Heroku Postgres!
-    }
-  }
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
