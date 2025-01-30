b0383d636ce26516a5d032804cd4410dd130c03a
VladimirGostik <xgostik@stuba.sk>
2025-01-30 21:00:59 +0100

    index.js opraveny

--- models/index.js
+++ models/index.js
@@ -1,25 +1,25 @@
-// models/index.js
 const Sequelize = require('sequelize');
 const sequelize = require('../config/database');
 
-const models = {};
+const db = {}; // Použi `db` namiesto `models` pre konzistentnosť
 
 // Inicializácia modelov
-models.Company = require('./Company')(sequelize, Sequelize.DataTypes);
-models.User = require('./User')(sequelize, Sequelize.DataTypes);
-models.Invoice = require('./Invoice')(sequelize, Sequelize.DataTypes);
-models.Service = require('./Service')(sequelize, Sequelize.DataTypes);
-models.MonthlyInvoice = require('./MonthlyInvoice')(sequelize, Sequelize.DataTypes);
-models.ServicePlanned = require('./ServicePlanned')(sequelize, Sequelize.DataTypes);
-models.Expense = require('./Expense')(sequelize, Sequelize.DataTypes);
+db.User = require('./User')(sequelize, Sequelize.DataTypes);
+db.Company = require('./Company')(sequelize, Sequelize.DataTypes);
+db.Invoice = require('./Invoice')(sequelize, Sequelize.DataTypes);
+db.Service = require('./Service')(sequelize, Sequelize.DataTypes);
+db.MonthlyInvoice = require('./MonthlyInvoice')(sequelize, Sequelize.DataTypes);
+db.ServicePlanned = require('./ServicePlanned')(sequelize, Sequelize.DataTypes);
+db.Expense = require('./Expense')(sequelize, Sequelize.DataTypes);
 
-// Pridaj Sequelize inštanciu do models objektu
-models.sequelize = sequelize;
-models.Sequelize = Sequelize;
+// Pridaj Sequelize inštanciu do db objektu
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-// Inicializácia asociácií
-Object.values(models)
+// Inicializácia asociácií (ak modely majú `associate` metódu)
+Object.values(db)
   .filter(model => typeof model.associate === 'function')
-  .forEach(model => model.associate(models));
+  .forEach(model => model.associate(db));
 
-module.exports = models;
+// Export databázového objektu so všetkými modelmi
+module.exports = db;
